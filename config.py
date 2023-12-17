a67ddc1558ebb9524d2e0253bc086a4e7ed23917
Ovi01 <63631702+Ovi01@users.noreply.github.com>
2023-12-18 04:25:17 +0500

    fix

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-bind = '192.168.1.103:80'
+bind = '0.0.0.0:80'
 log_dir = os.path.join(os.getcwd(), 'log')
 files_dir = os.path.join(os.getcwd(), 'static', 'uploads')
 files_url = '/static/uploads/'
